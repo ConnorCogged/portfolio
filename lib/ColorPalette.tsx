dd23454db84699cf6ed9aee29cdb6e6abbd788ee
ConnorCogged <Connorejohnson282@gmail.com>
2023-06-16 18:40:07 +0700

    Master (#1)
    * placeholder
    
    * placeholder

---
+++ lib/ColorPalette.tsx
@@ -0,0 +1,9 @@
+export let primaryColors = {
+    dark : {
+        primary : "bg-blue-600",
+        secondary : "bg-purple-600",
+        danger : "bg-red-600",
+        warning : "bg-yellow-500",
+        success : "bg-green-500",
+    }
+}
