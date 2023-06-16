1b141bd3dcffca2c03e3f9e081ffe413a11a8c03
Your Name <connorejohnson282@gmail.com>
2023-06-16 18:44:19 +0700

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
+//@ts-nocheck
+"use client"
 /* eslint-disable @next/next/no-head-element */
+
+import { useEffect, useRef } from 'react';
 import '../styles/globals.css'
 
 export default function RootLayout({
@@ -6,11 +10,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      const text = document.createElement("h5", "test")
+      text.parentElement = document.body
+    };
+
+    window.addEventListener("mousedown", handleClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleClick);
+    };
+  }, [ref]);
   return (
     <html>
       <head>
       </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import { primaryColors } from '../lib/ColorPalette';
 import { Button } from '@/ui/Button';
+import { useEffect, useRef } from "react";
 
 export default function Page() {
-  console.log(primaryColors.dark.secondary)
   return (
     <div>
-      <div>
-        <p className="cursive mb-4 mt-24 ml-16 text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-pink-500 via-red-500 to-yellow-500 background-animate">Connor Johnson</p>
-        <p className="text-white ml-16">Hey there! I'm a developer based in Thailand and have been developing for 4 years, but I started with basic programming concepts since the age of 7.</p>
+      <header className='text-center text-4xl font-extrabold'>Introduction</header>
+      <hr className='m-8'/>
+      <div id='introduction'>
+        <p className="cursive text-center text-6xl mt-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-pink-500 via-red-500 to-yellow-500 background-animate">Connor Johnson</p>
+        <p className="text-center">Hey there! I'm a developer based in Thailand and have been developing for <b className='pixel font-extrabold text-pink-400'>4 years</b>,<br/>but I started with basic programming concepts since the age of <b className='pixel font-extrabold text-green-400'>7</b>.</p>
       </div>
-      <div>
-        <p className="mt-12 ml-16 text-2xl font-extrabold text-white">Languages I can speak!</p>
+      <hr className='m-8'/>
+      <div id='languages'>
+        <header className="text-4xl text-center font-extrabold">Languages I can speak!</header>
       </div>
     </div>
   );
