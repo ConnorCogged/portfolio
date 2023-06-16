60c831fdcd029426989e916a0f3921ff199dcd9d
ConnorCogged <Connorejohnson282@gmail.com>
2023-06-16 18:58:40 +0700

    Master (#2)
    * placeholder
    
    * placeholder
    
    * init commit
    
    * init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
+//@ts-nocheck
+"use client"
 /* eslint-disable @next/next/no-head-element */
+
+import { useEffect, useRef } from 'react';
+=======
+/* eslint-disable @next/next/no-head-element */
+>>>>>>> main
 import '../styles/globals.css'
 
 export default function RootLayout({
@@ -6,11 +14,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+<<<<<<< HEAD
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      const text = document.createElement("h5", "test")
+      text.parentElement = document.body
+    };
+
+    window.addEventListener("mousedown", handleClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleClick);
+    };
+  }, [ref]);
+=======
+>>>>>>> main
   return (
     <html>
       <head>
       </head>
       <body>{children}</body>
     </html>
+<<<<<<< HEAD
+  )
+=======
   );
+>>>>>>> main
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import { primaryColors } from '../lib/ColorPalette';
 import { Button } from '@/ui/Button';
+<<<<<<< HEAD
+import { useEffect, useRef } from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <header className='mt-6 text-center text-4xl font-extrabold'>Introduction</header>
+      <hr className='m-8'/>
+      <div id='introduction'>
+        <p className="cursive text-center text-6xl mt-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-bl from-pink-500 via-red-500 to-yellow-500 background-animate">Connor Johnson</p>
+        <p className="text-center">Hey there! I'm a developer based in Thailand and have been developing for <b className='pixel font-extrabold text-pink-400'>4 years</b>,<br/>but I started with basic programming concepts since the age of <b className='pixel font-extrabold text-green-400'>7</b>.</p>
+      </div>
+      <hr className='m-8'/>
+      <div id='languages'>
+        <header className="text-4xl text-center font-extrabold">Languages I can speak!</header>
+=======
 
 export default function Page() {
   console.log(primaryColors.dark.secondary)
@@ -11,6 +27,7 @@ export default function Page() {
       </div>
       <div>
         <p className="mt-12 ml-16 text-2xl font-extrabold text-white">Languages I can speak!</p>
+>>>>>>> main
       </div>
     </div>
   );
