24471945d2eb825feb4e41f5f33e582ec4196548
ConnorCogged <Connorejohnson282@gmail.com>
2023-06-23 19:53:11 +0700

    Master (#5)
    * placeholder
    
    * placeholder
    
    * init commit
    
    * init commit
    
    * null
    
    * placeholder
    
    * placeholder

--- ui/Language.tsx
+++ ui/Language.tsx
@@ -5,13 +5,13 @@ export const Language = ({
     languageName : string,
     time : string,
 }) => {
-  let img = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${languageName.toLowerCase()}/${languageName.toLowerCase()}-original.svg`
+  let img = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${languageName.toLowerCase()}/${languageName.toLowerCase() == "tailwindcss" ? `${languageName.toLowerCase()}-original-wordmark` : `${languageName.toLowerCase()}-original`}.svg`
   console.log(img)
   return (
     <div className='align-top inline-block text-center'>
-        <img className='w-24 mt-4 ml-4 mr-4' src={img}/>
+        <img className='w-2/12 align-top inline-block text-center mx-auto' src={img}/>
         <p className='block text-sm font-thin'>{languageName}</p>
-        <p className='block text-2xl font-bold'>{time}</p>
+        <p className='block text-xl font-bold'>{time}</p>
     </div>
   );
 };
