a0193370d91e17455a3ef37d0a67a431fa9aba32
Your Name <connorejohnson282@gmail.com>
2023-06-23 22:57:05 +0700

    lolol

--- ui/Experience.tsx
+++ ui/Experience.tsx
@@ -10,11 +10,11 @@ export const Experience = ({
     time : string,
 }) => {
   return (
-    <div className="shadow-md rounded-xl h-64 m-4 w-2/4">
+    <div className="relative shadow-md rounded-xl m-4">
+        <span className="absolute bottom-0 -translate-x-1/2 left-1/2 text-gray-500 text-2xl">{time}</span>
         <h1 className="text-center pixel rounded-t-xl w-full bg-white text-xl">{children}</h1>
-        <img src={imagesrc} className="rounded-xl align-top h-2/6 mx-auto self-center justify-center mt-8"></img>
+        <img src={imagesrc} className="rounded-xl align-top h-2/6 mx-auto self-center justify-center mt-8 max-h-48"></img>
         <h1 className="mt-2 text-center">{description}</h1>
-        <h1 className="text-center text-gray-500 text-xl align-bottom inline">{time}</h1>
     </div>
   );
 };
